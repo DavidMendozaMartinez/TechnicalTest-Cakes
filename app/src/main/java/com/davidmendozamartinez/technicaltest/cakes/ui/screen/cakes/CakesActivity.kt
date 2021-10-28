b87fa6515535f761723544b45dcdfafc32c19106
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2021-10-28 12:20:09 +0200

    Create the Cake list screen

---
+++ app/src/main/java/com/davidmendozamartinez/technicaltest/cakes/ui/screen/cakes/CakesActivity.kt
@@ -0,0 +1,12 @@
+package com.davidmendozamartinez.technicaltest.cakes.ui.screen.cakes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.davidmendozamartinez.technicaltest.cakes.R
+
+class CakesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cakes)
+    }
+}
